1e62302c3604c59d1486b9d39ed9126cc7021794
Social App <@gmail.com>
2026-04-08 21:25:58 +0300

    update api url

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://social-app-production-7cfd.up.railway.app',
         changeOrigin: true,
         rewrite: (p) => p.replace(/^\/api/, ''),
       },
